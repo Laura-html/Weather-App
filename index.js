5e8fe788518168d8028be2f1cc2b635a19d89478
Laura-html <lrichards023@gmail.com>
2025-03-08 04:56:08 +0000

    Added api from source

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apiKey = "9245bb24b027319eb10oat71638b73cf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
